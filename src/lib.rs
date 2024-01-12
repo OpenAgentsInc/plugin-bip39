bf4f8103e3f080835d37dbdd3ad4be128efe7439
Christopher David <chris@arcadelabs.co>
2024-01-12 11:07:57 -0600

    Remove greet function

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,6 @@ use bip39::{Mnemonic, Language};
 const LANG: Language = Language::English;
 
 #[plugin_fn]
-pub fn greet(name: String) -> FnResult<String> {
-    Ok(format!("Hello, {}!", name))
-}
-
-#[plugin_fn]
 pub fn createMnemonic() -> FnResult<String> {
     let entropy = "7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f".as_bytes();
     let mnemonic = Mnemonic::from_entropy_in(LANG, &entropy).unwrap();
