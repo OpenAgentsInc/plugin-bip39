2319030ce8785779bcd0c58272c4309bcdde4bda
Christopher David <chris@arcadelabs.co>
2024-01-12 11:07:05 -0600

    Create mnemonic from demo entropy

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
 use extism_pdk::*;
+use bip39::{Mnemonic, Language};
+
+const LANG: Language = Language::English;
 
 #[plugin_fn]
 pub fn greet(name: String) -> FnResult<String> {
     Ok(format!("Hello, {}!", name))
+}
+
+#[plugin_fn]
+pub fn createMnemonic() -> FnResult<String> {
+    let entropy = "7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f7f".as_bytes();
+    let mnemonic = Mnemonic::from_entropy_in(LANG, &entropy).unwrap();
+
+    Ok(mnemonic.to_string())
 }
